2673a4e152a213734aac51393a085e5e89a45249
Jeffrey Stiles <freb@users.noreply.github.com>
2018-05-31 10:26:34 -0700

    add usable address total and workaround /32 issues

--- main.go
+++ main.go
@@ -15,9 +15,26 @@ func main() {
 		os.Exit(1)
 	}
 	total := 0
+	utotal := 0 //usable
 	for _, netaddr := range os.Args[1:] {
+
+		// testing. I should switch to the net.ParseCIDR parser.
+		// then use ipnet.Mask.Size() and use the second return, bits. guess i still
+		// need to convert to a string first...
+		//
+		// /32 doesn't display properly, but in ipsubnet lib.
+
+		// aip, aipnet, _ := net.ParseCIDR(netaddr)
+		// fmt.Println(aip, aipnet)
+		// aones, abits := aipnet.Mask.Size()
+		// fmt.Println(aones, abits)
+
+		// end testing
+
 		fields := strings.Split(netaddr, "/")
-		if len(fields) != 2 {
+		if len(fields) == 1 {
+			fields = append(fields, "32")
+		} else if len(fields) > 2 {
 			fmt.Println("malformed CIDR address:", netaddr)
 			continue
 		}
@@ -28,15 +45,34 @@ func main() {
 			continue
 		}
 		sub := ipsubnet.SubnetCalculator(ip, size)
+
+		if size == 32 {
+			fmt.Println(netaddr)
+			fmt.Println("------------------")
+			fmt.Println("Total IPs:        ", 1)
+			fmt.Println("Usable IPs:       ", 1)
+			fmt.Println("usage IP Range:   ", sub.GetIPAddressRange()[0], "-", sub.GetIPAddressRange()[0])
+			fmt.Println("Network Address:  ", sub.GetNetworkPortion())
+			fmt.Println("Broadcast Address:", sub.GetNetworkPortion())
+			fmt.Println("Subnet Mask:      ", sub.GetSubnetMask())
+			fmt.Println()
+			total += 1
+			utotal += 1
+			continue
+		}
+
 		fmt.Println(netaddr)
 		fmt.Println("------------------")
 		fmt.Println("Total IPs:        ", sub.GetNumberIPAddresses())
+		fmt.Println("Usable IPs:       ", sub.GetNumberAddressableHosts())
 		fmt.Println("usage IP Range:   ", sub.GetIPAddressRange()[0], "-", sub.GetIPAddressRange()[1])
 		fmt.Println("Network Address:  ", sub.GetNetworkPortion())
 		fmt.Println("Broadcast Address:", sub.GetBroadcastAddress())
 		fmt.Println("Subnet Mask:      ", sub.GetSubnetMask())
 		fmt.Println()
 		total += sub.GetNumberIPAddresses()
+		utotal += sub.GetNumberAddressableHosts()
 	}
-	fmt.Println("Total: ", total)
+	fmt.Println("Total:        ", total)
+	fmt.Println("Usable Total: ", utotal)
 }
